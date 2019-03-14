6f251ef26a12b2e42750a45ee3b81d55c678d369
linhs <linhaisheng@tulinggame.com>
2019-03-14 14:36:11 +0800

    管理输出：设定 HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,20 @@
 // option2: 在 package.json 中写个 npm script 
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { // 入口文件
     app: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [
+    // 如果我们更改了我们的一个入口起点的名称，甚至添加了一个新的名称，生成的包将被重命名在一个构建中
+    // 但是我们的index.html文件仍然会引用旧的名字。我们用 HtmlWebpackPlugin 来解决这个问题
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    })
+  ],
   output: {
     filename: '[name].bundle.js', // 输出文件名，其中 [name] 根据 entry 中的键值决定
     path: path.resolve(__dirname, 'dist'), // 输出路径
