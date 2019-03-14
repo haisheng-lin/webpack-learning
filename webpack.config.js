f67df18944b5d3301e8fe889f68c5836a400e27e
linhs <linhaisheng@tulinggame.com>
2019-03-14 14:47:49 +0800

    管理输出：清理 dist 目录

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@
 // option2: 在 package.json 中写个 npm script 
 
 const path = require('path');
+
+// 如果我们更改了我们的一个入口起点的名称，甚至添加了一个新的名称，生成的包将被重命名在一个构建中
+// 但是我们的 index.html 文件仍然会引用旧的名字。我们用 HtmlWebpackPlugin 来解决这个问题
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 由于 dist 目录在过去的指南和代码实例导致混乱不堪，webpack 会将打包文件放置在 dist 中
+// 但是 webpack 无法追踪哪些文件是实际项目中有用到的
+// 所以每次构建前都应该清理 dist 目录，是比较推荐的做法，而 clean-webpack-plugin 是当中比较普及的管理插件
+// 文档： https://github.com/johnagan/clean-webpack-plugin#options-and-defaults-optional
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { // 入口文件
@@ -11,8 +20,7 @@ module.exports = {
     print: './src/print.js',
   },
   plugins: [
-    // 如果我们更改了我们的一个入口起点的名称，甚至添加了一个新的名称，生成的包将被重命名在一个构建中
-    // 但是我们的index.html文件仍然会引用旧的名字。我们用 HtmlWebpackPlugin 来解决这个问题
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     })
