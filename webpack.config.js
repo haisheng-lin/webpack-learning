826ca5ef88574a34da8b3afb69a787bba5571f73
haisheng-lin <linhaicheng@xiao100.com>
2020-05-31 10:46:27 +0800

    feat: 增加 babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // option1: 使用 npx webpack --config webpack.config.js 打包构建
 // npx 的介绍可查阅: http://www.ruanyifeng.com/blog/2019/02/npx.html
-// option2: 在 package.json 中写个 npm script 
+// option2: 在 package.json 中写个 npm script
 
 const path = require('path');
 
@@ -15,7 +15,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { // 入口文件
+  entry: {
+    // 入口文件
     app: './src/index.js',
     print: './src/print.js',
   },
@@ -28,7 +29,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management',
-    })
+    }),
   ],
   output: {
     filename: '[name].bundle.js', // 输出文件名，其中 [name] 根据 entry 中的键值决定
@@ -37,17 +38,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
         test: /\.css$/, // 匹配以 '.css' 为后缀的文件
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/, // 匹配各种格式的图片
-        use: [
-          'file-loader',
-        ],
+        use: ['file-loader'],
       },
     ],
   },
